b7ad88135e256889c9d2a0e45e48f0709970e232
Sebastian <sre447@alumnos.unican.es>
2023-04-20 12:25:16 +0200

    Crear Practica5 y diagrama

---
+++ Practica5/Practica5/src/main/java/es/unican/is2/practica5/Movimiento.java
@@ -0,0 +1,34 @@
+package es.unican.is2.practica5;
+
+import java.time.LocalDateTime;
+
+public class Movimiento {
+	private String mConcepto;
+	private LocalDateTime mFecha;
+	private double mImporte;
+
+	public double getI() {
+		return mImporte;
+	}
+
+	public void setI(double newMImporte) {
+		mImporte = newMImporte;
+	}
+	
+	public String getC() {
+		return mConcepto;
+	}
+
+	public void setC(String newMConcepto) {
+		mConcepto = newMConcepto;
+	}
+
+	public LocalDateTime getF() {
+		return mFecha;
+	}
+
+	public void setF(LocalDateTime newMFecha) {
+		mFecha = newMFecha;
+	}
+	
+}
